9dfedebffa393c3da66b6b9db6e62095734c8417
Sim <simeon@twolivesleft.com>
2024-07-28 23:16:47 +0930

    Updates RuntimeKit with support for print handling

--- Frameworks/RuntimeKit.xcframework/ios-arm64_x86_64-simulator/RuntimeKit.framework/Headers/CodeaAddon.h
+++ Frameworks/RuntimeKit.xcframework/ios-arm64_x86_64-simulator/RuntimeKit.framework/Headers/CodeaAddon.h
@@ -15,29 +15,63 @@ struct lua_State;
 
 @protocol CodeaAddon <NSObject>
 
-//For registering your custom functions and libraries
+/// Called when the Lua state is first created. For registering your custom functions and libraries
+/// - Parameters:
+///   - controller: Runtime controller
+///   - L: Newly created Lua state
+///   - validating: Whether the state is being created to simply validate the current project
 - (void) codea:(ThreadedRuntimeViewController*)controller didCreateLuaState:(struct lua_State*)L isValidating:(BOOL)validating;
 
 @optional
 
-//For clean up (if necessary)
+/// Called just prior to the runtime being closed
+/// - Parameters:
+///   - controller: Runtime controller
+///   - L: Lua state about to be closed
 - (void) codea:(ThreadedRuntimeViewController*)controller willCloseLuaState:(struct lua_State*)L;
 
-//Handling changes to the viewer state (if necessary)
+/// Handling changes to the viewer state
+/// - Parameters:
+///   - controller: Runtime controller
+///   - pause: Whether the runtime is paused or unpaused
 - (void) codea:(ThreadedRuntimeViewController*)controller didPause:(BOOL)pause;
 
-//The reset button is pressed, this will cause:
-//  willCloseCodeaLuaState and didCreateCodeaLuaState to be called again in sequence
+/// The reset button is pressed, this will cause:
+/// willCloseCodeaLuaState and didCreateCodeaLuaState to be called again in sequence
+/// - Parameter controller: Runtime controller
 - (void) codeaWillRestart:(ThreadedRuntimeViewController*)controller;
 
-//Called each frame update
+/// Called each frame update
+/// - Parameters:
+///   - controller: Runtime controller
+///   - deltaTime: Time delta between this frame and the previous frame
 - (void) codeaWillDrawFrame:(ThreadedRuntimeViewController*)controller withDelta:(CGFloat)deltaTime;
 
-//Called after initial setup
+/// Called after initial setup
+/// - Parameter controller: Runtime controller
 - (void) codeaDidFinishSetup:(ThreadedRuntimeViewController*)controller;
 
-//Called when the addon is registered
+/// Called when the addon is registered
+/// - Parameter controller: Runtime controller
 - (void) codeaDidRegisterAddon:(ThreadedRuntimeViewController*)controller;
+
+/// Called when the runtime prints regular output
+/// - Parameters:
+///   - controller: Runtime controller
+///   - text: Output printed
+- (void) codea:(ThreadedRuntimeViewController*)controller didPrint:(NSString*)text;
+
+/// Called when the runtime prints a warning
+/// - Parameters:
+///   - controller: Runtime controller
+///   - text: Warning message
+- (void) codea:(ThreadedRuntimeViewController*)controller didWarn:(NSString*)text;
+
+/// Called when the runtime prints an error message
+/// - Parameters:
+///   - controller: Runtime controller
+///   - error: Error message output
+- (void) codea:(ThreadedRuntimeViewController*)controller didError:(NSString*)error;
 
 @end
 
